c4124704470a49e43c3d07561917ec666181a75e
Aleksey Korolev <im.playmean@gmail.com>
2022-06-26 22:37:54 +0300

    feat: ui server graceful shutdown

--- ui/server.go
+++ ui/server.go
@@ -3,6 +3,9 @@ package ui
 import (
 	"embed"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/filesystem"
@@ -16,6 +19,8 @@ type Server struct {
 	app          *fiber.App
 	workspace    *workspace.Workspace
 	externalVars map[string]string
+	osSignal     chan os.Signal
+	doneSignal   chan error
 }
 
 // TODO 24.06.2022 extend from error
@@ -34,14 +39,29 @@ func NewServer(w *workspace.Workspace, externalVars map[string]string) *Server {
 	s.app = app
 	s.workspace = w
 	s.externalVars = externalVars
+	s.osSignal = make(chan os.Signal)
+	s.doneSignal = make(chan error)
 
 	api := app.Group("/api")
 	api.Get("/version", s.methodGetVersion)
 	api.Get("/workspace", s.methodGetWorkspace)
 
+	signal.Notify(s.osSignal, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
+
 	return s
 }
 
 func (s *Server) Start(address string) error {
-	return s.app.Listen(address)
+	go func() {
+		err := s.app.Listen(address)
+
+		s.doneSignal <- err
+	}()
+
+	select {
+	case <-s.osSignal:
+		return nil
+	case err := <-s.doneSignal:
+		return err
+	}
 }
